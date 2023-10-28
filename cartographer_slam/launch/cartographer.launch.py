797d6b4dd282338c7057ff251bc83aaf32e6714a
Robert Gruberski <rgruberski@gmail.com>
2023-10-28 17:23:16 +0000

    add command line arguments with use_sim_time parameter, add yaml parameter rewrite to use the use_sim_time parameter value from the command line

--- cartographer_slam/launch/cartographer.launch.py
+++ cartographer_slam/launch/cartographer.launch.py
@@ -2,20 +2,30 @@ import os
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='True',
+        description='Use simulation clock if true')
 
     cartographer_config_dir = os.path.join(get_package_share_directory('cartographer_slam'), 'config')
     configuration_basename = 'cartographer.lua'
 
     return LaunchDescription([
-        
+        declare_use_sim_time_cmd,
+
         Node(
             package='cartographer_ros', 
             executable='cartographer_node', 
             name='cartographer_node',
             output='screen',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-configuration_directory', cartographer_config_dir,
                        '-configuration_basename', configuration_basename]),
 
@@ -24,7 +34,7 @@ def generate_launch_description():
             executable='occupancy_grid_node',
             output='screen',
             name='occupancy_grid_node',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-resolution', '0.05', '-publish_period_sec', '1.0']
         ),
     ]) 
--- localization_server/launch/localization.launch.py
+++ localization_server/launch/localization.launch.py
@@ -2,29 +2,60 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, TextSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='True',
+        description='Use simulation clock if true')
+
+    param_substitutions = {
+        'use_sim_time': use_sim_time
+    }
+
     map_file_arg = DeclareLaunchArgument(
         "map_file", default_value="warehouse_map_sim.yaml"
     )
 
-    map_file = PathJoinSubstitution([
+    map_yaml = PathJoinSubstitution([
         FindPackageShare('map_server'), 'config', LaunchConfiguration("map_file")
     ])
 
     nav2_yaml = os.path.join(get_package_share_directory('localization_server'), 'config', 'amcl_config.yaml')
+
+    urdf_file = '/home/user/simulation_ws/src/warehouse_robot_lab/rb1_base_common/rb1_base_description/robots/rb1_base.urdf'
+
+    with open(urdf_file, 'r') as info:
+        robot_desc = info.read()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc}]
+    )
 
     map_server_node = Node(
         package='nav2_map_server',
         executable='map_server',
         name='map_server',
         output='screen',
-        parameters=[{'use_sim_time': True}, 
-                        {'yaml_filename':map_file}]
+        parameters=[{'use_sim_time': use_sim_time}, 
+                    {'yaml_filename':map_yaml}]
+    )
+
+    amcl_node_config = RewrittenYaml(
+        source_file=nav2_yaml,
+        root_key='',
+        param_rewrites=param_substitutions,
+        convert_types=True
     )
 
     amcl_node = Node(
@@ -32,7 +63,7 @@ def generate_launch_description():
         executable='amcl',
         name='amcl',
         output='screen',
-        parameters=[nav2_yaml]
+        parameters=[amcl_node_config]
     )
 
     lifecycle_manager_node = Node(
@@ -40,12 +71,14 @@ def generate_launch_description():
         executable='lifecycle_manager',
         name='lifecycle_manager_mapper',
         output='screen',
-        parameters=[{'use_sim_time': True},
+        parameters=[{'use_sim_time': use_sim_time},
                     {'autostart': True},
                     {'node_names': ['map_server', 'amcl']}]
     )     
 
     return LaunchDescription([
+        declare_use_sim_time_cmd,
+        robot_state_publisher_node,
         map_file_arg,
         map_server_node,
         amcl_node,
